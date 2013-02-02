99ca618040b6c9f09232074515310d8447685798
Ben Jaffe <jaffe.ben@gmail.com>
2013-02-02 11:37:39 -0800

    1.1.39: Disabled Analytics

--- plugin/gaq.js
+++ plugin/gaq.js
@@ -1,12 +1,12 @@
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-37580424-1']);
-var loggedInUser = document.querySelector('#user_header .username') ? document.querySelector('#user_header .username').innerHTML : "not-signed-in";
-_gaq.push(['_trackPageview',loggedInUser]);
+// var _gaq = _gaq || [];
+// _gaq.push(['_setAccount', 'UA-37580424-1']);
+// var loggedInUser = document.querySelector('#user_header .username') ? document.querySelector('#user_header .username').innerHTML : "not-signed-in";
+// _gaq.push(['_trackPageview',loggedInUser]);
 
-// _gaq.push(['_trackEvent', 'PolyHide', 'Hid '+document.getElementById('ajax_gender').innerHTML]);
+// // _gaq.push(['_trackEvent', 'PolyHide', 'Hid '+document.getElementById('ajax_gender').innerHTML]);
 
-(function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
+// (function() {
+//   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+//   ga.src = 'https://ssl.google-analytics.com/ga.js';
+//   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+// })();
--- plugin/inject-ga-background.js
+++ plugin/inject-ga-background.js
@@ -1,43 +1,43 @@
 
-var ga_config_file = 'ga-config.js';
-// Keep an copy of ga.js in your extension, in case that the user
-//  does not have an internet connection, etc.
-var ga_code_file = 'ga.js';
+// var ga_config_file = 'ga-config.js';
+// // Keep an copy of ga.js in your extension, in case that the user
+// //  does not have an internet connection, etc.
+// var ga_code_file = 'ga.js';
 
-// Retrieve a previously saved copy, if available
-var ga_code = localStorage.getItem('ga_code') || '';
-var ga_url = 'https://ssl.google-analytics.com/ga.js';
+// // Retrieve a previously saved copy, if available
+// var ga_code = localStorage.getItem('ga_code') || '';
+// var ga_url = 'https://ssl.google-analytics.com/ga.js';
 
-var updateInterval = 2 * 60 * 60 * 1000; // Updates every 2 hours
+// var updateInterval = 2 * 60 * 60 * 1000; // Updates every 2 hours
 
-function updateGA() {
-    var x = new XMLHttpRequest();
-    x.onload = function() {
-        if (x.status == 200) {
-            console.log('Retrieved file. Size: ' + x.responseText.length);
-            ga_code = x.responseText;
-            // Cache code
-            localStorage.setItem('ga_code', ga_code);
-        } else console.log('Error retrieving ga.js; Status code ' + x.status);
-        if (updateInterval)  setTimeout(updateGA, updateInterval);
-    };
-    x.onerror = function() {
-        console.log('Error retrieving ga.js');
-        if (updateInterval)  setTimeout(updateGA, updateInterval);
-    };
-    x.open('GET', ga_url);
-    x.send();
-}
-// Trigger
-updateGA();
+// function updateGA() {
+//     var x = new XMLHttpRequest();
+//     x.onload = function() {
+//         if (x.status == 200) {
+//             console.log('Retrieved file. Size: ' + x.responseText.length);
+//             ga_code = x.responseText;
+//             // Cache code
+//             localStorage.setItem('ga_code', ga_code);
+//         } else console.log('Error retrieving ga.js; Status code ' + x.status);
+//         if (updateInterval)  setTimeout(updateGA, updateInterval);
+//     };
+//     x.onerror = function() {
+//         console.log('Error retrieving ga.js');
+//         if (updateInterval)  setTimeout(updateGA, updateInterval);
+//     };
+//     x.open('GET', ga_url);
+//     x.send();
+// }
+// // Trigger
+// updateGA();
 
-chrome.extension.onRequest.addListener(function(request, sender) {
-    if (request === 'INJECT_GA' && sender.tab) {
-        chrome.tabs.executeScript(sender.tab.id, {file: ga_config_file});
-        if (ga_code) {
-            chrome.tabs.executeScript(sender.tab.id, {code: ga_code});
-        } else {
-            chrome.tabs.executeScript(sender.tab.id, {file: ga_code_file});
-        }
-    }
-});
+// chrome.extension.onRequest.addListener(function(request, sender) {
+//     if (request === 'INJECT_GA' && sender.tab) {
+//         chrome.tabs.executeScript(sender.tab.id, {file: ga_config_file});
+//         if (ga_code) {
+//             chrome.tabs.executeScript(sender.tab.id, {code: ga_code});
+//         } else {
+//             chrome.tabs.executeScript(sender.tab.id, {file: ga_code_file});
+//         }
+//     }
+// });
