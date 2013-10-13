f44210a36f193e6e2c0452b8e4b79278a5be730a
Instant Green <instantgreen@server.fake>
2013-10-13 14:32:39 +0200

    My personal preference. Added category noArtist + questions.

--- plugin/defaultQuestions.js
+++ plugin/defaultQuestions.js
@@ -433,7 +433,29 @@ localStorage.okcpDefaultQuestions = JSON.stringify({
 		category: "science",
 		wrongAnswers:["Yes, and I practice one/some of these.","Yes, but I don't practice any of these."]
 	},
-
+	
+	//===== noArtist =====
+	{
+		qid:"158", //Are you an aspiring actor/noArtist/writer or other creative type?
+		category: "noArtist",
+		wrongAnswers:["Yes"]
+	},
+	{
+		qid:"1455", //Do you consider yourself an noArtist of any kind? (Music, painting, poetry, etc. all count.)
+		category: "noArtist",
+		wrongAnswers:["Yes"]
+	},
+	{
+		qid:"6619", //Do you feel Art has heavily influenced who you are?
+		category: "noArtist",
+		wrongAnswers:["Yes"]
+	},
+	{
+		qid:"1766", //Is art important to you?
+		category: "noArtist",
+		wrongAnswers:["Yes"]
+	},
+	
 	//===== children =====
 	{
 		qid:"80041", //Are you looking for a partner to have children with?
