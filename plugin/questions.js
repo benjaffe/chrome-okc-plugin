004d25c6b45ccec3890c7ffddc6deea9640a2954
Luke Madera <luke.madera@gmail.com>
2013-10-14 15:52:09 -0700

    ViewModels.js bug fix to find more match questions, add variable for number of requests to make. Modularize out questions to their own files and include in manifest which ones to use. Added a few more questions to match on. Attempt to add options page for questions (fail).

---
+++ plugin/questions.js
@@ -0,0 +1,2 @@
+var fileQuestions =[];
+// localStorage.fileQuestions ={};
---
+++ plugin/questions/bi.js
@@ -0,0 +1,59 @@
+//question search words: bisexual, sex, homosexual, gay, lesbian, gender
+fileQuestions.bi =
+	[
+		{
+			"qid":"1401",
+			"text":"Have you ever had a sexual encounter with someone of the same sex?",
+			"category": "bi",
+			"wrongAnswers":["No, and I would never.", "Yes, and I did not enjoy myself."]
+		},
+		
+		
+		{
+			"qid":"46563",
+			"text":"Assume you have a homosexual friend who is the same gender as you. Would it bother you if they hugged you?",
+			"category": "biOpen",
+			"wrongAnswers":["Yes.", "Only if I thought the hug was more than friendly."]
+		},
+		/*
+		{
+			"qid":"80928",
+			"text":"Have you ever seriously questioned your sexuality and whether it was different from what you first assumed?",
+			"category": "bi",
+			"wrongAnswers":[]
+		},
+		*/
+		
+		
+		{
+			"qid":"35203",
+			"text":"Would you consider being in a relationship with someone who has had homosexual sex?",
+			"category": "biOpen",
+			"wrongAnswers":["No"]
+		},
+		
+		{
+			"qid":"6258",
+			"text":"Would you date someone who is bisexual?",
+			"category": "biOpen",
+			"wrongAnswers":["No"]
+		},
+		{
+			"qid":"44384",
+			"text":"Which best represents your opinion of same-sex relationships?",
+			"category": "biOpen",
+			"wrongAnswers":["All same-sex relationships are wrong.", "Guy-on-guy is okay, but girl-on-girl is wrong.", "Girl-on-girl is okay, but guy-on-guy is wrong."]
+		},
+		{
+			"qid":"40484",
+			"text":"Would you date someone, knowing that they had a VERY close family member (parent, sibling) who was openly gay?",
+			"category": "biOpen",
+			"wrongAnswers":["No", "I'm Not Sure", "If they didn't flaunt it, it would be all right."]
+		},
+		{
+			"qid":"219",
+			"text":"Gay marriage -- should it be legal?",
+			"category": "biOpen",
+			"wrongAnswers":["No"]
+		}
+	];
