5dbf331b21f50140e298c93104275746cfca8824
Ben Jaffe <jaffe.ben@gmail.com>
2013-10-20 18:43:43 -0700

    Added Veg Questions

--- plugin/devTools.js
+++ plugin/devTools.js
@@ -1,11 +1,14 @@
 if (!!localStorage.devMode && JSON.parse(localStorage.devMode) === true) {
 	if (!!location.href.split('rqid=')[1]) {
-		console.log('hi');
 		var myArr = [], textToCopy;
 		$('.answer_area:first .my_answer').each(function(){
 			myArr.push($(this).parent().text());
 		});
-		textToCopy = '"answerText": ["' + myArr.join('", "') + '"],\n\t\t\t"score": [-0.5, 0, 1],\n\t\t\t"weight": [1, 1, 1]';
+		scoreWeightTempArr = [];
+		for (var i = myArr.length - 1; i >= 0; i--) {
+			scoreWeightTempArr.push(1);
+		}
+		textToCopy = '"answerText": ["' + myArr.join('", "') + '"],\n\t\t\t"score": ['+scoreWeightTempArr.join(', ')+'],\n\t\t\t"weight": ['+scoreWeightTempArr.join(', ')+']';
 		console.log(textToCopy);
 	}
 }
--- plugin/questions/veggie_vegan.js
+++ plugin/questions/veggie_vegan.js
@@ -6,20 +6,71 @@ fileQuestions.veg =
 			"qid":"179268",
 			"text":"Are you either vegetarian or vegan?",
 			"category": "veg",
-			"wrongAnswers":["Yes"]
+			"answerText": ["Yes", "No"],
+			"score": [1, -1]
 		},
 		{
 			"qid":"22198",
 			"text":"If you were out to dinner and your date was a vegetarian would you still order meat?",
 			"category": "veg",
-			"wrongAnswers":["I'm vegetarian too"]
+			"answerText": ["Yes", "No", "I'm Not Sure/ I would ask them first", "I'm vegetarian too"],
+			"score": [-1, 0, 0, 1]
 		},
 		{
 			"qid":"36345",
 			"text":"If an otherwise perfect match gave you an ultimatum to become a vegetarian, what would you do?",
 			"category": "veg",
-			"wrongAnswers":["Smile - I'm already a vegetarian."]		//"Comply - Become a vegetarian."
+			"answerText": ["Comply - Become a vegetarian.", "Rebel - This would be a deal breaker.", "Smile - I'm already a vegetarian."],
+			"score": [1, -1, 1]
+		},
+
+		//veg_friendly
+		{
+			"qid":"179268",
+			"text":"Are you either vegetarian or vegan?",
+			"category": "veg_friendly",
+			"answerText": ["Yes", "No"],
+			"score": [1, -1],
+			"weight": [1, 0]
+		},
+		{
+			"qid":"22198",
+			"text":"If you were out to dinner and your date was a vegetarian would you still order meat?",
+			"category": "veg_friendly",
+			"answerText": ["Yes", "No", "I'm Not Sure/ I would ask them first", "I'm vegetarian too"],
+			"score": [-1, 1, 1, 1],
+			"weight": [0.5, 1, 1, 1]
+		},
+		{
+			"qid":"36345",
+			"text":"If an otherwise perfect match gave you an ultimatum to become a vegetarian, what would you do?",
+			"category": "veg_friendly",
+			"answerText": ["Comply - Become a vegetarian.", "Rebel - This would be a deal breaker.", "Smile - I'm already a vegetarian."],
+			"score": [1, -1, 1],
+			"weight": [1, 1, 1]
+		},
+
+		//not_veg
+		{
+			"qid":"179268",
+			"text":"Are you either vegetarian or vegan?",
+			"category": "not_veg",
+			"answerText": ["Yes", "No"],
+			"score": [-1, 1]
+		},
+		{
+			"qid":"22198",
+			"text":"If you were out to dinner and your date was a vegetarian would you still order meat?",
+			"category": "not_veg",
+			"answerText": ["Yes", "No", "I'm Not Sure/ I would ask them first", "I'm vegetarian too"],
+			"score": [1, 1, 1, -1]
+		},
+		{
+			"qid":"36345",
+			"text":"If an otherwise perfect match gave you an ultimatum to become a vegetarian, what would you do?",
+			"category": "not_veg",
+			"answerText": ["Comply - Become a vegetarian.", "Rebel - This would be a deal breaker.", "Smile - I'm already a vegetarian."],
+			"score": [1, 1, -1]
 		}
 
-		//not veg
 	];
