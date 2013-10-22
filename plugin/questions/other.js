16d6d7a3f540f727bf57e1c7ab37fee9c8216a04
Ben Jaffe <jaffe.ben@gmail.com>
2013-10-21 19:58:48 -0700

    artist questions

--- plugin/questions/other.js
+++ plugin/questions/other.js
@@ -1,32 +1,63 @@
-fileQuestions.other = 
+fileQuestions.other =
 	[
 		//artist
-
-
-		//non-artist
 		{
 			"qid":"158",
-			"text":"Are you an aspiring actor/noArtist/writer or other creative type?",
-			"category": "noArtist",
-			"wrongAnswers":["Yes"]
+			"text":"Are you an aspiring actor/artist/writer or other creative type?",
+			"category": "artist",
+			"answerText": ["Yes", "No"],
+			"score": [1, -1]
 		},
 		{
 			"qid":"1455",
-			"text":"Do you consider yourself an noArtist of any kind? (Music, painting, poetry, etc. all count.)",
-			"category": "noArtist",
-			"wrongAnswers":["Yes"]
+			"text":"Do you consider yourself an artist of any kind? (Music, painting, poetry, etc. all count.)",
+			"category": "artist",
+			"answerText": ["Yes", "No"],
+			"score": [1, -1]
 		},
 		{
 			"qid":"6619",
 			"text":"Do you feel Art has heavily influenced who you are?",
-			"category": "noArtist",
-			"wrongAnswers":["Yes"]
+			"category": "artist",
+			"answerText": ["Yes", "No"],
+			"score": [1, -1]
 		},
 		{
 			"qid":"1766",
 			"text":"Is art important to you?",
-			"category": "noArtist",
-			"wrongAnswers":["Yes"]
+			"category": "artist",
+			"answerText": ["Yes", "No"],
+			"score": [1, -1]
 		},
+
+		//non_artist
+		{
+			"qid":"158",
+			"text":"Are you an aspiring actor/artist/writer or other creative type?",
+			"category": "non_artist",
+			"answerText": ["Yes", "No"],
+			"score": [-1, 1]
+		},
+		{
+			"qid":"1455",
+			"text":"Do you consider yourself an artist of any kind? (Music, painting, poetry, etc. all count.)",
+			"category": "non_artist",
+			"answerText": ["Yes", "No"],
+			"score": [-1, 1]
+		},
+		{
+			"qid":"6619",
+			"text":"Do you feel Art has heavily influenced who you are?",
+			"category": "non_artist",
+			"answerText": ["Yes", "No"],
+			"score": [-1, 1]
+		},
+		{
+			"qid":"1766",
+			"text":"Is art important to you?",
+			"category": "non_artist",
+			"answerText": ["Yes", "No"],
+			"score": [-1, 1]
+		}
 
 	];
