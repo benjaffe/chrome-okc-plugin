472fff3119bcf38d14bc441ff5f0bc151c4deb7c
Ben Jaffe <jaffe.ben@gmail.com>
2013-10-21 21:38:09 -0700

    relationship-related questions

--- plugin/questions/relationship_model.js
+++ plugin/questions/relationship_model.js
@@ -1,130 +1,265 @@
 fileQuestions.relationship_model =
 	[
-		//open to polyamory
+		//open_to_polyamory
 		{
 			"qid":"325",
 			"text":"Would you consider having an open relationship (i.e., one where you can see other people)?",
-			"category": "polyOpen",
-			"wrongAnswers":["No"]
+			"category": "open_to_polyamory",
+			"answerText": ["Yes", "No"],
+			"score": [1, -1],
+			"weight": [1, 1]
 		},
-		{
-			"qid":"41242",
-			"text":"Your significant other is traveling and has the opportunity to stay with a good friend that you know they find to be very attractive. What's your stance on the situation?",
-			"category": "polyOpen",
-			"wrongAnswers":["It would be totally unacceptable."]
-		},
-		{
-			"qid":"44540",
-			"text":"If you were in a serious relationship, would you mind if your significant other maintained an active profile on OkCupid?",
-			"category": "polyOpen",
-			"wrongAnswers":["Yes - I would mind this."]
-		},
-		// {
-		// 	"qid":"32",
-		// 	"text":"Group sex (with 3 or more people):",
-		// 	"category": "polyOpen",
-		// 	"wrongAnswers":["I have little or no interest."]
-		// },
-		{
-			"qid":"784",
-			"text":"Would you be okay with your significant other spending a lot of time with one of his/her exes (as a friend)?",
-			"category": "polyOpen",
-			"wrongAnswers":["No"]
-		},
-		{
-			"qid":"26557",
-			"text":"You're dating someone new. You discover that they have a room-mate the same sex as you. This:",
-			"category": "polyOpen",
-			"wrongAnswers":["Makes you feel uncomforatble"]
-		},
-		{
-			"qid":"84910",
-			"text":"Would it bother you if someone you are dating has a lot of close friends of the opposite sex?",
-			"category": "polyOpen",
-			"wrongAnswers":["Yes."]
-		},
-
-		
-		//polyamorous
 		{
 			"qid":"99709",
 			"text":"Do you consider yourself polyamorous?",
 			"category": "poly",
-			"wrongAnswers":["No"]
+			"answerText": ["Yes", "No", "I'm not sure", "No, but I consider myself non-monogamous"],
+			"score": [1, -1, 1, 1],
+			"weight": [1, 1, 1, 1]
 		},
-		{ //could also work with polyOpen
+		{
 			"qid":"131794",
 			"text":"How do you feel about polyamory? (multiple relationships)",
 			"category": "poly",
-			"wrongAnswers":["That's cheating, monogamy is the only way to go","I'm open to the concept, but its not for me"]
+			"answerText": ["That's cheating, monogamy is the only way to go", "I'm open to the concept, but its not for me", "Like swingers?", "I believe that polyamory can be fulfilling"],
+			"score": [-1, 1, 1, 1],
+			"weight": [1, 1, 0, 1]
 		},
 		{
-			"qid":"52827",
-			"text":"Would you consider connecting with someone whose relationship status is 'seeing someone' or 'married'?",
-			"category": "poly",
-			"answers":[],
-			"conclusiveAnswers":[],
-			"wrongAnswers":["No to both"]
+			"qid":"41242",
+			"text":"Your significant other is traveling and has the opportunity to stay with a good friend that you know they find to be very attractive. What's your stance on the situation?",
+			"category": "open_to_polyamory",
+			"answerText": ["It would be totally unacceptable.", "It might be ok, but I'd want to discuss it first.", "It's fine - I don't see any problem.", "It sounds sexy - Hopefully they'll hook up."],
+			"score": [-1, 1, 1, 1],
+			"weight": [0.6, 0.6, 0.6, 0.6]
 		},
-		{ //yes even in secret should fit a different category
+		{
+			"qid":"44540",
+			"text":"If you were in a serious relationship, would you mind if your significant other maintained an active profile on OkCupid?",
+			"category": "open_to_polyamory",
+			"answerText": ["Yes - I would mind this.", "No - This would not bother me."],
+			"score": [-1, 1],
+			"weight": [0.7, 0.7]
+		},
+		{
+			"qid":"784",
+			"text":"Would you be okay with your significant other spending a lot of time with one of his/her exes (as a friend)?",
+			"category": "open_to_polyamory",
+			"answerText": ["Yes", "No"],
+			"score": [1, -1],
+			"weight": [0.2, 0.2]
+		},
+		{
+			"qid":"26557",
+			"text":"You're dating someone new. You discover that they have a room-mate the same sex as you. This:",
+			"category": "open_to_polyamory",
+			"answerText": ["Makes you feel uncomfortable", "Doesn't faze you one bit"],
+			"score": [-1, 1],
+			"weight": [0.3, 0.3]
+		},
+		{
+			"qid":"84910",
+			"text":"Would it bother you if someone you are dating has a lot of close friends of the opposite sex?",
+			"category": "open_to_polyamory",
+			"answerText": ["Yes.", "No."],
+			"score": [-1, 1],
+			"weight": [0.3, 0.3]
+		},
+		{
 			"qid":"1128",
 			"text":"Would you date someone who was already in a committed relationship with someone else?",
 			"category": "poly",
-			"wrongAnswers":["Yes, even in secret.","No, it's wrong.","No, but I don't think it's inherently wrong."]
+			"answerText": ["Yes, even in secret.", "Yes, but only if everybody knew.", "No, but I don't think it's inherently wrong.", "No, it's wrong."],
+			"score": [1, 1, 1, -1],
+			"weight": [1, 1, 0.5, 1]
 		},
 		{
 			"qid":"16371",
 			"text":"Someone in an open relationship asks you out on a date. You:",
 			"category": "poly",
-			"wrongAnswers":["Refuse / Aren't interested in open relationships."]
+			"answerText": ["Refuse / Aren't interested in open relationships.", "Are hesitant, but would consider it.", "Accept / Are open to non-monogamous relationships."],
+			"score": [-1, 1, 1],
+			"weight": [1, 0.5, 1]
 		},
 		{
 			"qid":"33107",
 			"text":"Would you consider being part of a commited polyamorous relationship - ie, three or more people but no sex outside the group?",
 			"category": "poly",
-			"wrongAnswers":["I am commited to total monogamy"]
+			"answerText": ["Yes, I like that type of polygamy.", "I could be convinced by the right people", "I am commited to total monogamy", "I have open relationships only"],
+			"score": [1, 1, -1, 1],
+			"weight": [1, 0.6, 1, 1]
 		},
 		{
 			"qid":"48278",
 			"text":"Would you consider dating someone who is already involved in an open or polyamorous relationship?",
 			"category": "poly",
-			"wrongAnswers":["No."]
+			"answerText": ["Yes.", "No."],
+			"score": [1, -1],
+			"weight": [1, 1]
 		},
 		{
 			"qid":"423049",
 			"text":"If your partner told you they wanted to sleep with someone else, how would you react to that?",
 			"category": "poly",
-			"wrongAnswers":["That would be a deal breaker."]
+			"answerText": ["That would be a deal breaker.", "Positive or curious... tell me more!", "It depends on how I feel about that person.", "It depends on the sex of the person."],
+			"score": [-1, 1, 1, 1],
+			"weight": [0.5, 1, 1, 1]
 		},
 		{
 			"qid":"28742",
 			"text":"Is it okay for a married person to play around with someone with the permission of their spouse?",
 			"category": "poly",
-			"wrongAnswers":["No"]
+			"answerText": ["Yes", "No"],
+			"score": [1, -1],
+			"weight": [1, 1]
 		},
 		{
 			"qid":"18564",
 			"text":"Do you believe that it is possible to experience romantic love for more than one person at a time without loving one less because of your love for the other?",
 			"category": "poly",
-			"wrongAnswers":["No"]
+			"answerText": ["Yes", "No", "I'm Not Sure"],
+			"score": [1, -1, 1],
+			"weight": [1, 1, 0.6]
 		},
 		{
 			"qid":"47098",
 			"text":"Would you consider dating someone who likes to maintain more than one sexual partnership, but keeps these relationships separate from one another? In other words ... Multiple partners, but only one at a time.",
 			"category": "poly",
-			"wrongAnswers":["No."]
+			"answerText": ["Yes.", "No."],
+			"score": [1, -1],
+			"weight": [1, 1]
 		},
 		{
 			"qid":"36",
 			"text":"Would you ever consider an open marriage? That means you can sleep with other people.",
 			"category": "poly",
-			"wrongAnswers":["No"]
+			"answerText": ["Yes", "No"],
+			"score": [1, -1],
+			"weight": [1, 1]
+		},
+
+
+		
+		//polyamorous
+		{
+			"qid":"325",
+			"text":"Would you consider having an open relationship (i.e., one where you can see other people)?",
+			"category": "open_to_polyamory",
+			"answerText": ["Yes", "No"],
+			"score": [1, -1],
+			"weight": [0.6, 0.6]
+		},
+		{
+			"qid":"99709",
+			"text":"Do you consider yourself polyamorous?",
+			"category": "poly",
+			"answerText": ["Yes", "No", "I'm not sure", "No, but I consider myself non-monogamous"],
+			"score": [1, -1, 0, 1],
+			"weight": [1, 1, 1, 1]
+		},
+		{
+			"qid":"131794",
+			"text":"How do you feel about polyamory? (multiple relationships)",
+			"category": "poly",
+			"answerText": ["That's cheating, monogamy is the only way to go", "I'm open to the concept, but its not for me", "Like swingers?", "I believe that polyamory can be fulfilling"],
+			"score": [-1, -1, 1, 1],
+			"weight": [1, 1, 0, 1]
+		},
+		{
+			"qid":"52827",
+			"text":"Would you consider connecting with someone whose relationship status is 'seeing someone' or 'married'?",
+			"category": "poly",
+			"answerText": ["Yes to both", "No to both", "Yes to 'seeing someone' only", "Yes to 'married' only"],
+			"score": [1, -1, 1, 1],
+			"weight": [0.6, 0.6, 0.6, 0.6]
+		},
+		{
+			"qid":"1128",
+			"text":"Would you date someone who was already in a committed relationship with someone else?",
+			"category": "poly",
+			"answerText": ["Yes, even in secret.", "Yes, but only if everybody knew.", "No, but I don't think it's inherently wrong.", "No, it's wrong."],
+			"score": [1, 1, 0, -1],
+			"weight": [1, 1, 1, 1]
+		},
+		{
+			"qid":"16371",
+			"text":"Someone in an open relationship asks you out on a date. You:",
+			"category": "poly",
+			"answerText": ["Refuse / Aren't interested in open relationships.", "Are hesitant, but would consider it.", "Accept / Are open to non-monogamous relationships."],
+			"score": [-1, 0.5, 1],
+			"weight": [1, 1, 1]
+		},
+		{
+			"qid":"33107",
+			"text":"Would you consider being part of a commited polyamorous relationship - ie, three or more people but no sex outside the group?",
+			"category": "poly",
+			"answerText": ["Yes, I like that type of polygamy.", "I could be convinced by the right people", "I am commited to total monogamy", "I have open relationships only"],
+			"score": [1, 1, -1, 1],
+			"weight": [1, 1, 1, 1]
+		},
+		{
+			"qid":"48278",
+			"text":"Would you consider dating someone who is already involved in an open or polyamorous relationship?",
+			"category": "poly",
+			"answerText": ["Yes.", "No."],
+			"score": [1, -1],
+			"weight": [1, 1]
+		},
+		{
+			"qid":"423049",
+			"text":"If your partner told you they wanted to sleep with someone else, how would you react to that?",
+			"category": "poly",
+			"answerText": ["That would be a deal breaker.", "Positive or curious... tell me more!", "It depends on how I feel about that person.", "It depends on the sex of the person."],
+			"score": [-1, 1, 1, 1],
+			"weight": [1, 1, 0.8, 0.8]
+		},
+		{
+			"qid":"28742",
+			"text":"Is it okay for a married person to play around with someone with the permission of their spouse?",
+			"category": "poly",
+			"answerText": ["Yes", "No"],
+			"score": [1, -1],
+			"weight": [1, 1]
+		},
+		{
+			"qid":"18564",
+			"text":"Do you believe that it is possible to experience romantic love for more than one person at a time without loving one less because of your love for the other?",
+			"category": "poly",
+			"answerText": ["Yes", "No", "I'm Not Sure"],
+			"score": [1, -1, 0],
+			"weight": [1, 1, 1]
+		},
+		{
+			"qid":"47098",
+			"text":"Would you consider dating someone who likes to maintain more than one sexual partnership, but keeps these relationships separate from one another? In other words ... Multiple partners, but only one at a time.",
+			"category": "poly",
+			"answerText": ["Yes.", "No."],
+			"score": [1, -1],
+			"weight": [1, 1]
+		},
+		{
+			"qid":"36",
+			"text":"Would you ever consider an open marriage? That means you can sleep with other people.",
+			"category": "poly",
+			"answerText": ["Yes", "No"],
+			"score": [1, 0],
+			"weight": [1, 1]
 		},
 		{
 			"qid":"37772",
 			"text":"If you had to choose one for the rest of your life, which would you pick?",
 			"category": "poly",
-			"wrongAnswers":["Monogamy"]
+			"answerText": ["Monogamy", "An open relationship", "Polyamory", "Playing the field"],
+			"score": [-1, 1, 1, 0.5],
+			"weight": [1, 1, 1, 0.5]
+		},
+		{
+			"qid":"1121",
+			"text":"Have you ever had multiple romantic partners during the same time period?",
+			"category": "communication",
+			"answerText": ["Yes, and they both knew.", "Yes, and I didn't tell at least one of them.", "No."],
+			"score": [1, 1, 0],
+			"weight": [1, 1, 0.5]
 		},
 
 		//monogamous
@@ -135,38 +270,54 @@ fileQuestions.relationship_model =
 			"qid":"1121",
 			"text":"Have you ever had multiple romantic partners during the same time period?",
 			"category": "communication",
-			"wrongAnswers":["Yes, and I didn't tell at least one of them."]
+			"answerText": ["Yes, and they both knew.", "Yes, and I didn't tell at least one of them.", "No."],
+			"score": [1, -1, 0],
+			"weight": [1, 1, 0]
 		},
 
 
-		//not possessive
+		//not_possessive
 		{
 			"qid":"1440",
 			"text":"Jealousy: healthy or unhealthy, in the context of a relationship?",
-			"category": "notPossessive",
-			"wrongAnswers":["Healthy"]
+			"category": "not_possessive",
+			"answerText": ["Healthy", "Unhealthy"],
+			"score": [-1, 1],
+			"weight": [1, 1]
 		},
 		{
 			"qid":"784",
 			"text":"Would you be okay with your significant other spending a lot of time with one of his/her exes (as a friend)?",
-			"category": "notPossessive",
-			"wrongAnswers":["No"]
+			"category": "not_possessive",
+			"answerText": ["Yes", "No"],
+			"score": [1, -1],
+			"weight": [1, 1]
 		},
 		{
 			"qid":"31581",
 			"text":"Your significant other's ex is coming into town and he/she wants to go out to dinner with them alone. How do you react?",
-			"category": "notPossessive",
-			"wrongAnswers":["No way.","Not thrilled, but go ahead."]
+			"category": "not_possessive",
+			"answerText": ["Sure, no problem.", "Not thrilled, but go ahead.", "No way."],
+			"score": [1, 0, -1],
+			"weight": [1, 1, 1]
 		},
 
-		//wanting children
-
-
-		//not wanting children
+		//wanting_children
 		{
 			"qid":"80041", //Are you looking for a partner to have children with?
-			"category": "children",
-			"wrongAnswers":["Yes"]
+			"category": "wanting_children",
+			"answerText": ["Yes", "No"],
+			"score": [1, -1],
+			"weight": [1, 1]
+		},
+
+		//not_wanting_children
+		{
+			"qid":"80041", //Are you looking for a partner to have children with?
+			"category": "not_wanting_children",
+			"answerText": ["Yes", "No"],
+			"score": [-1, 1],
+			"weight": [1, 1]
 		}
 
 		//traditional conservative
