7db0d8baa78616ca2639e89143138df9df1c19e9
Ben Jaffe <jaffe.ben@gmail.com>
2013-10-21 19:53:37 -0700

    devTools improved for question adding

--- plugin/devTools.js
+++ plugin/devTools.js
@@ -1,6 +1,8 @@
 if (!!localStorage.devMode && JSON.parse(localStorage.devMode) === true) {
-	if (!!location.href.split('rqid=')[1]) {
+	var qid = location.href.split('rqid=')[1];
+	if (!!qid) {
 		var myArr = [], textToCopy;
+		var answerText = $('#qtext_'+qid).text();
 		$('.answer_area:first .my_answer').each(function(){
 			myArr.push($(this).parent().text());
 		});
@@ -8,7 +10,7 @@ if (!!localStorage.devMode && JSON.parse(localStorage.devMode) === true) {
 		for (var i = myArr.length - 1; i >= 0; i--) {
 			scoreWeightTempArr.push(1);
 		}
-		textToCopy = '"answerText": ["' + myArr.join('", "') + '"],\n\t\t\t"score": ['+scoreWeightTempArr.join(', ')+'],\n\t\t\t"weight": ['+scoreWeightTempArr.join(', ')+']';
+		textToCopy = '"qid":"'+qid+'",\n\t\t\t"text":"'+answerText+'",\n\t\t\t"answerText": ["' + myArr.join('", "') + '"],\n\t\t\t"score": ['+scoreWeightTempArr.join(', ')+'],\n\t\t\t"weight": ['+scoreWeightTempArr.join(', ')+']';
 		console.log(textToCopy);
 	}
 }
