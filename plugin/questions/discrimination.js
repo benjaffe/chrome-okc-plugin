d26f46831d8b23a4701fd29b0d1c021cb5b4d800
Ben Jaffe <jaffe.ben@gmail.com>
2013-10-21 19:54:41 -0700

    Race and Weight

--- plugin/questions/discrimination.js
+++ plugin/questions/discrimination.js
@@ -5,18 +5,21 @@ fileQuestions.discrimination =
 			"qid":"123",
 			"text":"Would you strongly prefer to go out with someone of your own skin color / racial background?",
 			"answerText": ["Yes", "No"],
+			"category": "race",
 			"score": [-1, 1]
 		},
 		{
 		"qid":"46927",
 			"text":"Would you consider dating someone who has vocalized a strong negative bias toward a certain race of people?",
 			"answerText": ["Yes.", "No.", "It depends on which race."],
+			"category": "race",
 			"score": [-1, 1, -1]
 		},
 		{
 			"qid":"55744",
 			"text":"If you were going to have a child, would you want the other parent to be of the same ethnicity as you?",
 			"answerText": ["Yes.", "No.", "This would not be an important factor to me."],
+			"category": "race",
 			"score": [-1, 1, 1]
 		},
 
@@ -33,8 +36,8 @@ fileQuestions.discrimination =
 		{
             "qid":"52682",
             "text":"If one of your potential matches were overweight, would that be a dealbreaker?",
-            "category": "weight",
             "answerText": ["Yes, even if they were slightly overweight.", "Yes, but only if they were obese.", "No.", "No, in fact I prefer overweight people."],
-			"score": [-1, -1, 1, 1]
+			"category": "weight",
+            "score": [-1, -1, 1, 1]
 		}
 	];
