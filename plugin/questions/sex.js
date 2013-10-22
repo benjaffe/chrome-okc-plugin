c96f5088b9a53f5efd583f08504ed3fec86ac9ec
Ben Jaffe <jaffe.ben@gmail.com>
2013-10-22 09:05:59 -0700

    sex-related questions

--- plugin/questions/sex.js
+++ plugin/questions/sex.js
@@ -5,109 +5,159 @@ fileQuestions.sex =
 			"qid":"48753",
 			"text":"Do you like to cuddle?",
 			"category": "cuddling",
-			"wrongAnswers":["No.","Sometimes - It depends."]
+			"answerText": ["Yes.", "No.", "Sometimes - It depends."],
+			"score": [1, -1, 0],
+			"weight": [1, 1, 1] 
 		},
-		// {
-		// 	"qid":"19378",
-		// 	"text":"How are you most likely to show your partner you care?",
-		// 	"category": "cuddling",
-		// 	"wrongAnswers":["Gifts: Buy them flowers, jewelery or a new gadget","Actions: Do something nice (like a date or errand)","Words: Tell them how much you appreciate them"]
-		// },
+		{
+			"qid":"19378",
+			"text":"How are you most likely to show your partner you care?",
+			"category": "cuddling",
+			"answerText": ["Words: Tell them how much you appreciate them", "Actions: Do something nice (like a date or errand)", "Gifts: Buy them flowers, jewelery or a new gadget", "Touch: Give them a hug, massage or kiss"],
+			"score": [0, 0, 0, 1],
+			"weight": [0, 0, 0, 1] 
+		},
 		{
 			"qid":"72318",
 			"text":"Would you consider inviting someone to bed with you for clearly-stated non-sexual purposes (e.g., cuddle and sleep only) on a first date?",
 			"category": "cuddling",
-			"wrongAnswers":["No."]
+			"answerText": ["Yes.", "No."],
+			"score": [1, -1],
+			"weight": [1, 1] 
 		},
 		{
 			"qid":"41850",
 			"text":"How important is it for you to make physical contact when showing affection for someone?",
 			"category": "cuddling",
-			"wrongAnswers":["Not important.","Somewhat important."]
+			"answerText": ["Very important.", "Somewhat important.", "Not important."],
+			"score": [1, 0, -1],
+			"weight": [1, 1, 1] 
 		},
 
-		//love priority
+		//love_over_sex
 		{
 			"qid":"35",
 			"text":"Regardless of future plans, what's more interesting to you right now?",
-			"category": "love",
-			"wrongAnswers":["Sex"]
+			"category": "love_over_sex",
+			"answerText": ["Sex", "Love"],
+			"score": [-1, 1],
+			"weight": [1, 1] 
 		},
 		{
 			"qid":"14913",
 			"text":"Did you join OkCupid just so you could find people to have sex with?",
-			"category": "love",
-			"wrongAnswers":["Yes"]
+			"category": "love_over_sex",
+			"answerText": ["Yes", "No"],
+			"score": [-1, 1],
+			"weight": [1, 0.5] 
 		},
 		{
 			"qid":"41953",
 			"text":"About how long do you want your next relationship to last?",
-			"category": "love",
-			"wrongAnswers":["One night","A few months to a year"]
+			"category": "love_over_sex",
+			"answerText": ["One night", "A few months to a year", "Several years", "The rest of my life"],
+			"score": [-1, 0.3, 0.6, 1],
+			"weight": [1, 0.5, 0.8, 1] 
 		},
 		{
 			"qid":"20135",
 			"text":"How do you feel about falling in love?",
-			"category": "love",
-			"wrongAnswers":["I try to avoid it","I'm indifferent / not sure"]
+			"category": "love_over_sex",
+			"answerText": ["I love it and want it very much", "I try to avoid it", "I like to just let it happen", "I'm indifferent / not sure"],
+			"score": [1, -1, 1, 0],
+			"weight": [1, 1, 0.5, 1] 
 		},
+
+		//group_sex
+		// {
+		// 	"qid":"32",
+		// 	"text":"Group sex (with 3 or more people):",
+		// 	"category": "group_sex",
+		// 	"wrongAnswers":["I have little or no interest."]
+		// },
 
 		//fetish
 		{
 			"qid":"67511",
 			"text":"Suppose you\'re dating someone who seems to have long-term potential. You discover that they want to urinate on you during sex. Would you consider staying with this person?",
 			"category": "fetish",
-			"wrongAnswers":["No."]
+			"answerText": ["Yes.", "No."],
+			"score": [1, -1],
+			"weight": [1, 0.2] 
 		},
 		{
 			"qid":"665",
 			"text":"Are you fetish-friendly?",
 			"category": "fetish",
-			"wrongAnswers":["Ew!"]
+			"answerText": ["Bring out the toys!", "Never tried it, but I'm open-minded.", "Not sure/don't know what they really are.", "Ew!"],
+			"score": [1, 1, 0, -1],
+			"weight": [1, 1, 1, 1] 
 		},
 
-		//kinky
+		//rough_kinky
 		{
 			"qid":"18530",
 			"text":"Do you want your partner to be kinkier than you?",
-			"category": "kinky",
-			"wrongAnswers":["No"]
+			"category": "rough_kinky",
+			"answerText": ["Yes", "No", "Not possible"],
+			"score": [1, -1, 1],
+			"weight": [1, 1, 1] 
 		},
 		{
 			"qid":"1028",
 			"text":"Is your ideal sex rough or gentle?",
-			"category": "kinky",
-			"wrongAnswers":["Gentle","I'm a virgin"]
+			"category": "rough_kinky",
+			"answerText": ["Rough", "Gentle", "I enjoy both equally", "I'm a virgin"],
+			"score": [1, -1, 1, -1],
+			"weight": [1, 0.5, 1, 1] 
 		},
 		{
 			"qid":"72086",
 			"text":"If your partner needed lovemaking to always be gentle, would you be fine with this?",
-			"category": "kinky",
-			"wrongAnswers":["Yes."]
+			"category": "rough_kinky",
+			"answerText": ["Yes.", "No."],
+			"score": [-1, 1],
+			"weight": [1, 1] 
 		},
 		{
 			"qid":"9628",
 			"text":"Biting?",
-			"category": "kinky",
-			"wrongAnswers":["No"]
-		},
-		{
-			"qid":"1133",
-			"text":"Do you have rape fantasies?",
-			"category": "kinky",
-			"wrongAnswers":["No"]
+			"category": "rough_kinky",
+			"answerText": ["No", "Yes", "Let's break skin"],
+			"score": [-1, 0.5, 1],
+			"weight": [1, 1, 1] 
 		},
 		{
 			"qid":"30115",
 			"text":"Have you ever gotten, or would you ever get, a piercing below the belt?",
-			"category": "kinky",
-			"wrongAnswers":["I wouldn't."]
+			"category": "rough_kinky",
+			"answerText": ["I have.", "I would.", "I wouldn't."],
+			"score": [1, 1, -1],
+			"weight": [1, 1, 0.4] 
+		},
+		{
+			"qid":"1133",
+			"text":"Do you have rape fantasies?",
+			"category": "rough_kinky",
+			"answerText": ["Yes", "No"],
+			"score": [1, -1],
+			"weight": [1, 0.2] 
 		},
 		{
 			"qid":"58812",
 			"text":"Would you consider roleplaying out a rape fantasy with partner who asked you to?",
-			"category": "kinky",
-			"wrongAnswers":["No."]
+			"category": "rough_kinky",
+			"answerText": ["Yes.", "No."],
+			"score": [1, -1],
+			"weight": [1, 0.3] 
+		},
+		{
+			"qid":"1011",
+			"text":"Do you know what a 'safeword' is, in a sexual context?",
+			"category": "rough_kinky",
+			"answerText": ["Yes", "No"],
+			"score": [1, -1],
+			"weight": [1, 1] 
 		},
 
 
@@ -116,13 +166,17 @@ fileQuestions.sex =
 			"qid":"20",
 			"text":"BDSM: Without looking it up, do you know exactly what it stands for?",
 			"category": "BDSM",
-			"wrongAnswers":["No"]
+			"answerText": ["Yes", "No"],
+			"score": [1, -1],
+			"weight": [1, 1] 
 		},
 		{
 			"qid":"1011",
 			"text":"Do you know what a 'safeword' is, in a sexual context?",
 			"category": "BDSM",
-			"wrongAnswers":["No."]
+			"answerText": ["Yes", "No"],
+			"score": [1, -1],
+			"weight": [1, 1] 
 		},
 
 
@@ -132,13 +186,25 @@ fileQuestions.sex =
 			"qid":"30",
 			"text":"Would you like to receive pain during sex?",
 			"category": "masochistic",
-			"wrongAnswers":["No"]
+			"answerText": ["Yes, lots and lots", "Yes, some", "No"],
+			"score": [1, 0.3, -1],
+			"weight": [1, 1, 1] 
 		},
 		{
 			"qid":"28545",
 			"text":"When having sex, do you like to have your hair pulled?",
 			"category": "masochistic",
-			"wrongAnswers":["No way.","Don't know / Not sure."]
+			"answerText": ["Yes, and hard!", "Yes, but gently.", "No way.", "Don't know / Not sure."],
+			"score": [1, 0.3, -1, 0],
+			"weight": [1, 1, 1, 0.5] 
+		},
+		{
+			"qid":"1011",
+			"text":"Do you know what a 'safeword' is, in a sexual context?",
+			"category": "masochistic",
+			"answerText": ["Yes", "No"],
+			"score": [1, -1],
+			"weight": [1, 1] 
 		},
 
 		//dominant
@@ -146,59 +212,101 @@ fileQuestions.sex =
 			"qid":"61733",
 			"text":"Would you be pleased if a partner expressed the desire to be sexually humiliated by you?",
 			"category": "dominant",
-			"wrongAnswers":["No."]
+			"answerText": ["Yes.", "No."],
+			"score": [1, -1],
+			"weight": [1, 1] 
 			
 		},
-
+		{
+			"qid":"463",
+			"text":"In your ideal sexual encounter, do you take control, or do they?",
+			"category": "dominant",
+			"answerText": ["I take control", "They take control"],
+			"score": [1, -1],
+			"weight": [1, 1] 
+		},
+		{
+			"qid":"83808",
+			"text":"Would you ever consider a relationship where you would take on an exclusive sexual role as master or slave?",
+			"category": "dominant",
+			"answerText": ["Yes, as the master only.", "Yes, as the slave only.", "Yes, as the master or the slave.", "No."],
+			"score": [1, -1, 1, -1],
+			"weight": [1, 1, 0.7, 1] 
+		},
+		
 
 		//submissive
 		{
 			"qid":"463",
 			"text":"In your ideal sexual encounter, do you take control, or do they?",
 			"category": "submissive",
-			"wrongAnswers":["I take control"]
+			"answerText": ["I take control", "They take control"],
+			"score": [-1, 1],
+			"weight": [1, 1] 
 		},
 		{
 			"qid":"60726",
 			"text":"If a trusted partner asked you to submit to them sexually, would you? Assume that this would involve letting them collar you, command you, and have control over you during sex.",
 			"category": "submissive",
-			"wrongAnswers":["No."]
+			"answerText": ["Yes.", "No."],
+			"score": [1, -1],
+			"weight": [1, 0.5] 
 		},
 		{
 			"qid":"38320",
 			"text":"Is it generally acceptable to you for a sex partner to initiate foreplay while you are sleeping?",
 			"category": "submissive",
-			"wrongAnswers":["No."]
+			"answerText": ["Yes.", "No."],
+			"score": [1, -1],
+			"weight": [1, 1] 
 		},
 		{
 			"qid":"83808",
 			"text":"Would you ever consider a relationship where you would take on an exclusive sexual role as master or slave?",
 			"category": "submissive",
-			"wrongAnswers":["No","Yes, as the master only."]
+			"answerText": ["Yes, as the master only.", "Yes, as the slave only.", "Yes, as the master or the slave.", "No."],
+			"score": [-1, 1, 1, -1],
+			"weight": [1, 1, 0.7, 1] 
 		},
 		{
 			"qid":"79635",
 			"text":"How would you feel if someone called you \"good girl\" or \"good boy\" during sex?",
 			"category": "submissive",
-			"wrongAnswers":["Negative.","Indifferent."]
+			"answerText": ["Positive.", "Negative.", "Indifferent.", "It would depend on which of the two I was called."],
+			"score": [1, -1, 0, 1],
+			"weight": [1, 1, 0.5, 1] 
 		},
 		{
 			"qid":"11",
 			"text":"How does the idea of being slapped hard in the face during sex make you feel?",
 			"category": "submissive",
-			"wrongAnswers":["Horrified","Indifferent"]
+			"answerText": ["Horrified", "Aroused", "Nostalgic", "Indifferent"],
+			"score": [-1, 1, 1, 0],
+			"weight": [1, 1, 0.7, 0.6] 
 		},
 		{
 			"qid":"84005",
 			"text":"As an adult, have you ever worn a leash and collar in public?",
 			"category": "submissive",
-			"wrongAnswers":["No."]
+			"answerText": ["Yes.", "No."],
+			"score": [1, -1],
+			"weight": [1, 0.2] 
 		},
 		{
 			"qid":"48347",
 			"text":"Do you think you could ever enjoy being humiliated as part of a sexual experience?",
 			"category": "submissive",
-			"wrongAnswers":["No."]
+			"answerText": ["Yes.", "No."],
+			"score": [1, -1],
+			"weight": [1, 0.6] 
+		},
+		{
+			"qid":"1011",
+			"text":"Do you know what a 'safeword' is, in a sexual context?",
+			"category": "bondage",
+			"answerText": ["Yes", "No"],
+			"score": [1, -1],
+			"weight": [1, 1] 
 		},
 
 
@@ -208,13 +316,25 @@ fileQuestions.sex =
 			"qid":"29",
 			"text":"Would you rather...",
 			"category": "bondage",
-			"wrongAnswers":["avoid bondage all together","do the tying"]
+			"answerText": ["be tied up during sex", "do the tying", "avoid bondage all together"],
+			"score": [1, 1, -1],
+			"weight": [1, 1, 1] 
 		},
 		{
 			"qid":"1134",
 			"text":"Do you have a desire (even if it's secret) to take part in sexual activities involving bondage?",
 			"category": "bondage",
-			"wrongAnswers":["No","Absolutely not."]
+			"answerText": ["Yes", "No", "Absolutely not."],
+			"score": [1, 0, -1],
+			"weight": [1, 0.5, 1] 
+		},
+		{
+			"qid":"1011",
+			"text":"Do you know what a 'safeword' is, in a sexual context?",
+			"category": "bondage",
+			"answerText": ["Yes", "No"],
+			"score": [1, -1],
+			"weight": [1, 1] 
 		},
 
 
@@ -223,27 +343,45 @@ fileQuestions.sex =
 			"qid":"1040",
 			"text":"Receiving anal sex?",
 			"category": "anal",
-			"wrongAnswers":["I don't like it / I don't think I would like it"]
+			"answerText": ["I like it / I think I might like it", "I don't like it / I don't think I would like it"],
+			"score": [1, -1],
+			"weight": [1, 1] 
 		},
 		{
 			"qid":"18600",
 			"text":"People who like RECEIVING anal sex are...",
 			"category": "anal",
-			"wrongAnswers":["Perverts","Beyond my comprehension"]
+			"answerText": ["Exploring their sexuality to the full", "Perverts", "Beyond my comprehension"],
+			"score": [1, -1, -1],
+			"weight": [1, 1, 1] 
 		},
 		{
 			"qid":"64476",
 			"text":"Under the right circumstances, would you allow a partner to lick your anus?",
 			"category": "anal",
-			"wrongAnswers":["No."]
+			"answerText": ["Yes.", "No."],
+			"score": [1, -1],
+			"weight": [1, 1] 
 		},
 
-		//shaved
+		//shaven
 		{
 			"qid":"58829",
 			"text":"When it comes to your pubic hair, do you make a regular effort to maintain its appearance (or lack thereof)?",
-			"category": "shaved",
-			"wrongAnswers":["No."]
+			"category": "shaven",
+			"answerText": ["Yes.", "No."],
+			"score": [1, -1],
+			"weight": [1, 1] 
+		},
+
+		//unshaven
+		{
+			"qid":"58829",
+			"text":"When it comes to your pubic hair, do you make a regular effort to maintain its appearance (or lack thereof)?",
+			"category": "unshaven",
+			"answerText": ["Yes.", "No."],
+			"score": [-1, 1],
+			"weight": [0.5, 1] 
 		},
 
 	];
