c4e27b6a4d6913aa360dff7616c362b0d8304b09
Ben Jaffe <jaffe.ben@gmail.com>
2013-10-22 17:09:04 -0700

    Science Spirituality Questions

--- plugin/questions/science_spirituality.js
+++ plugin/questions/science_spirituality.js
@@ -1,15 +1,19 @@
 fileQuestions.science_spirituality =
 	[
-		//scientific literacy
+		//science_literacy
 		{
 			"qid":"409", //A "shooting star" is a star that...
-			"category": "science",
-			"wrongAnswers":["...burned out, and collapsed","...collided with Earth's atmosphere","...got sucked into a black hole"]
+			"category": "science_literacy",
+			"answerText": ["...burned out, and collapsed", "...collided with Earth's atmosphere", "...got sucked into a black hole", "...isn't really a star"],
+			"score": [-1, -1, -1, 1],
+			"weight": [1, 1, 1, 1]
 		},
 		{
 			"qid":"178", //Which is bigger?
-			"category": "science",
-			"wrongAnswers":["The earth"]
+			"category": "science_literacy",
+			"answerText": ["The earth", "The sun"],
+			"score": [-1, 1],
+			"weight": [1, 1]
 		},
 		
 		//science-friendly
@@ -17,162 +21,231 @@ fileQuestions.science_spirituality =
 			"qid":"612",
 			"text":"Should evolution and creationism be taught side-by-side in schools?",
 			"category": "science",
-			"wrongAnswers":["No, evolution has no place in schools", "Yes, students should hear both sides"]
+			"answerText": ["Yes, students should hear both sides", "No, creationism has no place in schools", "No, evolution has no place in schools"],
+			"score": [-0.5, 1, -1],
+			"weight": [0.5, 1, 1]
 		},
 		{
 			"qid":"15889",
 			"text":"Do you put more weight in science or faith?",
 			"category": "science",
-			"wrongAnswers":["Faith"]
+			"answerText": ["Science", "Faith", "Equally in both"],
+			"score": [1, -1, 0],
+			"weight": [1, 1, 1]
 		},
 		{
 			"qid":"126793",
 			"text":"How do you believe the universe most likely came into existence?",
 			"category": "science",
-			"wrongAnswers":["God or gods created it within the last 10k years","God or gods created it a very long time ago"]
+			"answerText": ["God or gods created it within the last 10k years", "God or gods created it a very long time ago", "The universe has an entirely naturalistic origin", "The universe has always existed"],
+			"score": [-1, -1, 1, 1],
+			"weight": [1, 1, 1, 1]
 		},
 		{
 			"qid":"258",
 			"text":"Did America really put a man on the moon?",
 			"category": "science",
-			"wrongAnswers":["No"]
+			"answerText": ["Yes", "No"],
+			"score": [1, -1],
+			"weight": [0.8, 0.8]
 		},
 		{
 			"qid":"19898",
 			"text":"Do you enjoy finding out what makes things work the way they do?",
 			"category": "science",
-			"wrongAnswers":["As long as it works, who cares why?"]
-		},
-		{
-			"qid":"20408",
-			"text":"Do you believe in an energy field that surrounds us that we can manipulate through reiki, acupuncture, chakra balancing, yoga, or some other system?",
-			"category": "science",
-			"wrongAnswers":["Yes, and I practice one/some of these.","Yes, but I don't practice any of these."]
+			"answerText": ["I'm an information sponge", "Sometimes", "As long as it works, who cares why?"],
+			"score": [1, 0.5, -1],
+			"weight": [1, 1, 1]
 		},
 
 
-		//logic
+		//quizzes
 		{
 			"qid":"255",
 			"text":"STALE is to STEAL as 89475 is to...",
-			"category": "logic",
-			"wrongAnswers":["89457","98547","89754"]
+			"category": "quizzes",
+			"answerText": ["89457", "98547", "89754", "89547"],
+			"score": [-1, -1, -1, 1],
+			"weight": [1, 1, 1, 1]
 		},
 		{
 			"qid":"511",
 			"text":"If you flipped three pennies, what would be the odds that they all came out the same?",
-			"category": "logic",
-			"wrongAnswers":["I admit, I don't know!","1 in 3","1 in 4"]
-		},
-		{
-			"qid":"178",
-			"text":"Which is bigger?",
-			"category": "logic",
-			"wrongAnswers":["The earth"]
+			"category": "quizzes",
+			"answerText": ["I admit, I don't know!", "1 in 3", "1 in 4", "1 in 8"],
+			"score": [0.5, -1, -1, 1],
+			"weight": [1, 1, 1, 1]
 		},
 		{
 			"qid":"8672",
 			"text":"In the line Wherefore art thou Romeo?, what does wherefore mean?",
-			"category": "logic",
-			"wrongAnswers":["Where","How","Who cares / wtf?"]
+			"category": "quizzes",
+			"answerText": ["Why", "Where", "How", "Who cares / wtf?"],
+			"score": [1, -1, -1, -1],
+			"weight": [1, 1, 1, 1]
 		},
 		{
 			"qid":"1201",
 			"text":"What is next in this series? 1, 4, 10, 19, 31, _",
-			"category": "logic",
-			"wrongAnswers":["36","48","Don't know / don't care"]
+			"category": "quizzes",
+			"answerText": ["36", "48", "46", "Don't know / don't care"],
+			"score": [-1, -1, 1, -0.5],
+			"weight": [1, 1, 1, 1]
 		},
 		{
 			"qid":"14835",
 			"text":"If you turn a left-handed glove inside out, it fits...",
-			"category": "logic",
-			"wrongAnswers":["On my left hand"]
+			"category": "quizzes",
+			"answerText": ["On my left hand", "On my right hand"],
+			"score": [-1, 1],
+			"weight": [1, 1]
 		},
 
 
 		//spiritual
-
-
-		//non-spiritual
 		{
-			"qid":"763",
-			"text":"Do you believe in karma?",
-			"category": "notSpiritual",
-			"wrongAnswers":["Yes"]
+			"qid":"20408",
+			"text":"Do you believe in an energy field that surrounds us that we can manipulate through reiki, acupuncture, chakra balancing, yoga, or some other system?",
+			"category": "spiritual",
+			"answerText": ["Yes, and I practice one/some of these.", "Yes, but I don't practice any of these.", "No, but I do practice one/some of these.", "No, and I would never practice any of these."],
+			"score": [1, 1, -1, -1],
+			"weight": [1, 1, 1, 1]
 		},
 		{
 			"qid":"210",
 			"text":"Do you believe in God?",
-			"category": "notSpiritual",
-			"wrongAnswers":["Yes"]
+			"category": "spiritual",
+			"answerText": ["Yes", "No"],
+			"score": [1, 0],
+			"weight": [1, 0]
+		},
+		{
+			"qid":"763",
+			"text":"Do you believe in karma?",
+			"category": "spiritual",
+			"answerText": ["Yes", "No"],
+			"score": [1, -1],
+			"weight": [1, 1]
+		},
+
+		//non-spiritual
+		{
+			"qid":"20408",
+			"text":"Do you believe in an energy field that surrounds us that we can manipulate through reiki, acupuncture, chakra balancing, yoga, or some other system?",
+			"category": "non-spiritual",
+			"answerText": ["Yes, and I practice one/some of these.", "Yes, but I don't practice any of these.", "No, but I do practice one/some of these.", "No, and I would never practice any of these."],
+			"score": [-1, -1, 1, 1],
+			"weight": [1, 1, 1, 1]
+		},
+		{
+			"qid":"210",
+			"text":"Do you believe in God?",
+			"category": "non-spiritual",
+			"answerText": ["Yes", "No"],
+			"score": [-1, 1],
+			"weight": [1, 0.3]
+		},
+		{
+			"qid":"763",
+			"text":"Do you believe in karma?",
+			"category": "non-spiritual",
+			"answerText": ["Yes", "No"],
+			"score": [-1, 1],
+			"weight": [1, 1]
 		},
 
 		//religious
-
-
-		//non-religious
+		{
+			"qid":"210",
+			"text":"Do you believe in God?",
+			"category": "religious",
+			"answerText": ["Yes", "No"],
+			"score": [1, -1],
+			"weight": [1, 0.5]
+		},
 		{
 			"qid":"156913",
 			"text":"Are you Christian?",
-			"category": "nonReligious",
-			"wrongAnswers":["Yes"]
+			"category": "religious",
+			"answerText": ["Yes", "No"],
+			"score": [1, 0],
+			"weight": [1, 0]
 		},
 		{
 			"qid":"156915",
 			"text":"Are you a Buddhist?",
-			"category": "nonReligious",
-			"wrongAnswers":["Yes"]
+			"category": "religious",
+			"answerText": ["Yes", "No"],
+			"score": [1, 0],
+			"weight": [1, 0]
 		},
 		{
 			"qid":"156914",
 			"text":"Are you Jewish?",
-			"category": "nonReligious",
-			"wrongAnswers":["Yes"]
+			"category": "religious",
+			"answerText": ["Yes", "No"],
+			"score": [1, 0],
+			"weight": [1, 0]
 		},
 		{
 			"qid":"156916",
 			"text":"Are you Muslim?",
-			"category": "nonReligious",
-			"wrongAnswers":["Yes"]
-		},
-		{
-			"qid":"44",
-			"text":"Some religions are more correct than others.",
-			"category": "nonReligious",
-			"wrongAnswers":["True"]
+			"category": "religious",
+			"answerText": ["Yes", "No"],
+			"score": [1, 0],
+			"weight": [1, 0]
 		},
 
-
-		/*
+		//non-religious
 		{
-			"qid":"126793",
-			"text":"How do you believe the universe most likely came into existence?",
-			"category": "science",
-			"wrongAnswers":["God or gods created it within the last 10k years","God or gods created it a very long time ago"]
+			"qid":"210",
+			"text":"Do you believe in God?",
+			"category": "non-religious",
+			"answerText": ["Yes", "No"],
+			"score": [-1, 1],
+			"weight": [1, 1]
 		},
 		{
-			"qid":"409",
-			"text":"A 'shooting star' is a star that...",
-			"category": "science",
-			"wrongAnswers":["...burned out, and collapsed","...collided with Earth's atmosphere","...got sucked into a black hole"]
+			"qid":"156913",
+			"text":"Are you Christian?",
+			"category": "non-religious",
+			"answerText": ["Yes", "No"],
+			"score": [-1, 0],
+			"weight": [1, 0]
 		},
 		{
-			"qid":"19898",
-			"text":"Do you enjoy finding out what makes things work the way they do?",
-			"category": "science",
-			"wrongAnswers":["As long as it works, who cares why?"]
+			"qid":"156915",
+			"text":"Are you a Buddhist?",
+			"category": "non-religious",
+			"answerText": ["Yes", "No"],
+			"score": [-1, 0],
+			"weight": [1, 0]
 		},
 		{
-			"qid":"301",
-			"text":"Are you annoyed by people who are super logical?",
-			"category": "science",
-			"wrongAnswers":["Yes"]
+			"qid":"156914",
+			"text":"Are you Jewish?",
+			"category": "non-religious",
+			"answerText": ["Yes", "No"],
+			"score": [-1, 0],
+			"weight": [1, 0]
 		},
 		{
-			"qid":"20408",
-			"text":"Do you believe in an energy field that surrounds us that we can manipulate through reiki, acupuncture, chakra balancing, yoga, or some other system?",
-			"category": "science",
-			"wrongAnswers":["Yes, and I practice one/some of these.","Yes, but I don't practice any of these."]
+			"qid":"156916",
+			"text":"Are you Muslim?",
+			"category": "non-religious",
+			"answerText": ["Yes", "No"],
+			"score": [-1, 0],
+			"weight": [1, 0]
 		}
-		*/
+
+
+		// {
+		// 	"qid":"44",
+		// 	"text":"Some religions are more correct than others.",
+		// 	"category": "tolerant",
+		// 	"score": [1, 1],
+		// 	"weight": [1, 1]
+		// },
+
+
 	];
