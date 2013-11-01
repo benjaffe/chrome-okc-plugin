2ebd47f401d38c8c49494496c1ec7aad01521d01
Ben Jaffe <jaffe.ben@gmail.com>
2013-10-31 19:35:26 -0700

    Re-added pagination in questions and messages lists

--- plugin/readdPagination.js
+++ plugin/readdPagination.js
@@ -1,14 +1,28 @@
 // setTimeout(function(){
-	var lastPageElem = $('#questions .pages .count .last');
-	var numPages = lastPageElem.text()*1;
-	var paginationElem = $('<div class="pagination"></div>').insertAfter(lastPageElem);
-	for (var i = 0; i < numPages; i++) {
-		var newElem = lastPageElem.clone();
-		var href = newElem.attr('href');
-		href = href.split('low=')[0]+'low='+i+'1&n=0';
+(function() {
+	var lastPageElem,
+		numPages,
+		itemsPerPage,
+		paginationElem;
+	if (document.getElementById('questions')) {
+		lastPageElem = $('#questions .count .last');
+		itemsPerPage = 10;
+	} else if (document.getElementById('p_mailbox')) {
+		lastPageElem = $('#p_mailbox .count .last');
+		itemsPerPage = 30;
+	} else {
+		return false;
+	}
+
+	numPages = lastPageElem.text()*1;
+	paginationElem = $('<div class="pagination"></div>').insertAfter(lastPageElem);
+	for (var i = 0, newElem, href; i < numPages; i++) {
+		newElem = lastPageElem.clone();
+		href = newElem.attr('href');
+		href = href.split('low=')[0]+'low=' + (i*itemsPerPage+1) + '&' + href.split('&')[1];
 		newElem.text(i+1)
 			.attr('href',href)
 			.attr('class','num');
 		paginationElem.append(newElem);
 	}
-// },1000);
+})();
