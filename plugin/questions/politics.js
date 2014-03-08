84d3717b46e3834682d06f84f384112d900c8aea
Evan Carroll <me@evancarroll.com>
2014-03-07 21:03:45 -0600

    Made new catagory, "class consciousness." Marx and I wish the radical ladies out there the best of luck.

---
+++ plugin/questions/politics.js
@@ -0,0 +1,209 @@
+_OKCP.fileQuestions.politics =
+	{
+		"class_conscious": [
+			{
+				"qid": "214",
+				"text": "Overall, has capitalism made the world a better place?",
+				"answerText": ["Yes", "No"],
+				"score": [-1, 1],
+				"weight": [1, 1]
+			},
+			{
+				"qid": "174",
+				"text": "Communism, in concept:",
+				"answerText": ["Good" ,"Bad" ,"Same as capitalism" ,"No idea / this question doesn't interest me"],
+				"score": [1, -1, -1, 0],
+				"weight": [1, 1, 0.5, 1]
+			},
+			{
+				"qid": "19215",
+				"text": "Could you date someone who has (and acts on) radical-left politics?",
+				"answerText": [ "Yes" , "No way, you commie" , "As long as it's non-violent." , "Not sure" ],
+				"score": [1, -1, 1, 0],
+				"weight": [1, 1, 0.5, 1]
+			},
+			{
+				"qid": "18555",
+				"text": "Have you ever felt so strongly about a social issue or cause that you have participated in a public demonstration?",
+				"answerText": ["Yes", "No", "Politics bore me"],
+				"score": [1, -1, 0],
+				"weight": [1, 1, 1]
+			},
+			{
+				"qid": "36327",
+				"text": "Would you like to live in a small community where everyone lives and works together, and where all property and possessions are owned collectively?",
+				"answerText": [ "Yes." , "No." , "Perhaps, depending upon the specifics." , "I already do!" ],
+				"score": [1, -1, 1, 1],
+				"weight": [1, 1, 0.5, 5]
+			},
+			{
+				"qid": "283",
+				"text": "Would you give the police significantly more power, if it would lower crime rates?",
+				"answerText": [ "Yes" , "No" ],
+				"score": [-1, 1],
+				"weight": [1, 1]
+			},
+			{
+				"qid": "37314",
+				"text": 'Which of the following "solutions" do you think would provide the biggest reduction in crime rates?',
+				"answerText": [ "More and better trained police" , "More and better armed civilians" , "More and better implemented social programs" , "More severe punishment for convicted criminals" ],
+				"score": [-1, 1, 1, -1],
+				"weight": [1, 0.5, 1, 0]
+			},
+			{
+				"qid": "37314",
+				"text": "Which would you rather choose: peace on Earth or loads of money?",
+				"answerText": [ "Peace on Earth", "Loads of money" ],
+				"score": [1, -1],
+				"weight": [1, 1]
+			},
+			{
+				"qid": "50570",
+				"text": "If there were no welfare programs available in your community, which would you choose to help?",
+				"answerText": [ "Give money directly to poor people." , "Give money to the government." , "Give money to a charity." , "Keep my money for myself, friends, and/or family." ],
+				"score": [1, -1, 1, -1],
+				"weight": [1, 1, 1, 1]
+			},
+			{
+				"qid": "486",
+				"text": "Is a welfare system a good idea? Or does taxing for it just take money away from hard-working people who earned it?",
+				"answertext": [ "Welfare is mostly good", "Welfare is mostly bad" ],
+				"score": [1, -1],
+				"weight": [1, 1]
+			},
+			{
+				"qid": "48328",
+				"text": "Do you believe that money can buy happiness?",
+				"answertext": [ "Yes.", "No." ],
+				"score": [-1, 1],
+				"weight": [1, 1]
+			},
+			{
+				"qid": "58660",
+				"text": "Is there any amount of money that could persuade you to murder someone whom you know nothing about?",
+				"answertext": [ "Yes.", "No." ],
+				"score": [-1, 1],
+				"weight": [1, 1]
+			},
+			{
+				"qid": "15312",
+				"text": "Is it acceptable for a country to spend more money on its defense program, military, or war budget than on its education or welfare programs?",
+				"answertext": [ "Yes", "No", "I'm Not Sure" ],
+				"score": [-1, 1, 0],
+				"weight": [1, 1, 1]
+			},
+			{
+				"qid": "9385",
+				"text": "Overall, what is a more valuable and worthy goal for society?",
+				"answertext": [ "Liberty", "Equality" ],
+				"score": [0, 1],
+				"weight": [1, 1]
+			},
+			{
+				"qid": "34113",
+				"text": "How do you feel about government-subsidized food programs (free lunch, food stamps, etc.)?",
+				"answertext": [ "No problem" , "It's ok - if it is not abused" , "Ok for short amounts of time" , "Never - Get a job" ],
+				"score": [1, 0, 0, -1],
+				"weight": [1, 1, 1, 1]
+			},
+			{
+				"qid": "55981",
+				"text": "If a company contacted you as a reference for a friend who is applying for a job, would you consider lying to help your friend get the job?",
+				"answertext": [ "Yes.", "No." ],
+				"score": [ 1, -1 ],
+				"weight": [ 1, 1 ]
+			},
+			{
+				"qid": "38492",
+				"text": "Would you be willing to lie on a job resume or application if you thought it would get you the job and you were sure you wouldn't get caught?",
+				"answertext": [ "Yes.", "No." ],
+				"score": [ 1, -1 ],
+				"weight": [ 1, 1 ]
+			},
+			{
+				"qid": "38492",
+				"text": "Are you the type of person to tell a homeless person to get a job?",
+				"answertext": [ "Yes", "No" ],
+				"score": [ 1, 1 ],
+				"weight": [ -1, 1 ]
+			},
+			{
+				"qid": "172",
+				"text": "Would you fight for your country in an unjust war?",
+				"answertext": [ "Yes", "No" ],
+				"score": [ -1, 1 ],
+				"weight": [ 1, 1 ]
+			},
+			{
+				"qid": "9678",
+				"text": "Would you risk your life to protest against an unjust government?",
+				"answertext": [ "Yes", "No" ],
+				"score": [ 1, -1 ],
+				"weight": [ 1, 1 ]
+			},
+			{
+				"qid": "175",
+				"text": "Should burning your country's flag be illegal?",
+				"answertext": [ "Yes", "No" ],
+				"score": [ -1, 1 ],
+				"weight": [ 1, 1 ]
+			},
+			{
+				"qid": "175",
+				"text": "Which is more offensive: book burning or flag burning?",
+				"answertext": [ "book burning", "flag burning" ],
+				"score": [ 1, -1 ],
+				"weight": [ 1, 1 ]
+			},
+			{
+				"qid": "85158",
+				"text": "Would you support a system that would allow people to be free to live and work in any country in the world regardless of their citizenship or nation of birth?",
+				"answertext": [ "Yes.", "No.", "I'm not sure." ],
+				"score": [ 1, -1, 0 ],
+				"weight": [ 1, 1, 1 ]
+			},
+			{
+				"qid": "85155",
+				"text": "Should customs officials be allowed to search computer data in travelers' laptops at border crossings?",
+				"answertext": [ "Yes.", "No." ],
+				"score": [ -1, 1 ],
+				"weight": [ 1, 1 ]
+			},
+			{
+				"qid": "85159",
+				"text": "In order to reduce crime, is it more effective to mandate harsher punishments or tackle the social and economic causes?",
+				"answertext": [ "Mandate harsher punishments.", "Tackle the causes." ],
+				"score": [ -1, 1 ],
+				"weight": [ 1, 1 ]
+			},
+			{
+				"qid": "212813",
+				"text": "Which best describes your political beliefs?",
+				"answertext": [ "Liberal / Left-wing", "Centrist" , "Conservative / Right-wing" , "Other" ],
+				"score": [ 1, -1, -1, 1 ],
+				"weight": [ 1, 1, 2, 1 ]
+			},
+			{
+				"qid": "85380",
+				"text": "Should laws prohibit companies from drug testing employees who are in non-safety critical jobs?",
+				"answertext": [ "Yes.", "No." ],
+				"score": [ 1, -1 ],
+				"weight": [ 1, 1 ]
+			},
+			{
+				"qid": "15876",
+				"text": "Is separation of church and state important to you?",
+				"answertext": [ "Yes", "No" ],
+				"score": [ 1, -1 ],
+				"weight": [ 1, 1 ]
+			},
+			{
+				"qid": "19221",
+				"text": "What is your attitude toward service-industry/hospitality professionals (waitstaff, bellhops, valets, etc.)?",
+				"answertext": [ "They deserve their tips" , "They need to get a real job" , "It's a good way to start your career" , "They're so beneath me" ],
+				"score": [ 1, -1, -1, -1 ],
+				"weight": [ 1, 1, 1, 1 ]
+			}
+		]
+	};
+
