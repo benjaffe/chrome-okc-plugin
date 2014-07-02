fa758d3019c8b069d6f98920712521a70fcbb906
Jamie Bliss <astronouth7303@gmail.com>
2014-07-02 15:33:42 -0400

    Map questions-init.js to new settings storage

--- plugin/questions-init.js
+++ plugin/questions-init.js
@@ -1,41 +1,41 @@
-
-var questions = {
-	"questionsVersionNum": "1.5.0",
-	"questionsList": []
-};
-
-var xx, yy;
-var fullQuestionsList = {};
 _OKCP.filteredQuestionsList = {};
-var desiredCategories = JSON.parse(localStorage.okcp).questionCategories;
+// XXX: Do we need a promise for this value?
 
-for(xx in _OKCP.fileQuestions) {
-	for (yy in _OKCP.fileQuestions[xx]) {
-		if (_OKCP.fileQuestions[xx].hasOwnProperty(yy)) {
-			fullQuestionsList[yy] = _OKCP.fileQuestions[xx][yy];
+_OKCP.storage.settings.get(['questionCategories'])
+.then(function(data) {
+	var xx, yy;
+	var fullQuestionsList = {};
+	var desiredCategories = data.desiredCategories;
+	for(xx in _OKCP.fileQuestions) {
+		for (yy in _OKCP.fileQuestions[xx]) {
+			if (_OKCP.fileQuestions[xx].hasOwnProperty(yy)) {
+				fullQuestionsList[yy] = _OKCP.fileQuestions[xx][yy];
+			}
 		}
 	}
-}
-_OKCP.fileQuestions = fullQuestionsList;
-_OKCP.categoryList = [];
-_OKCP.fullQuestionsList = [];
-//loop through all questions
-for (xx in fullQuestionsList) {
-	//push category name into the category list.
-	_OKCP.categoryList.push(xx);
-	//push category questions into fullQuestionsList
-	for (yy in fullQuestionsList[xx]) {
-		_OKCP.fullQuestionsList.push(fullQuestionsList[xx][yy]);
-	}
-	//loop through desired categories
-	for (var i = desiredCategories.length - 1; i >= 0; i--) {
-		//if the current questions matches, use it
-		if (xx === desiredCategories[i]) {
-			_OKCP.filteredQuestionsList[xx] = fullQuestionsList[xx];
+	_OKCP.fileQuestions = fullQuestionsList;
+	_OKCP.categoryList = [];
+	_OKCP.fullQuestionsList = [];
+	//loop through all questions
+	for (xx in fullQuestionsList) {
+		//push category name into the category list.
+		_OKCP.categoryList.push(xx);
+		//push category questions into fullQuestionsList
+		for (yy in fullQuestionsList[xx]) {
+			_OKCP.fullQuestionsList.push(fullQuestionsList[xx][yy]);
 		}
+		//loop through desired categories
+		for (var i = desiredCategories.length - 1; i >= 0; i--) {
+			//if the current questions matches, use it
+			if (xx === desiredCategories[i]) {
+				_OKCP.filteredQuestionsList[xx] = fullQuestionsList[xx];
+			}
 
+		}
 	}
-}
 
-questions.questionsList = _OKCP.filteredQuestionsList;
-localStorage.okcpDefaultQuestions = JSON.stringify(questions);
+	localStorage.okcpDefaultQuestions = JSON.stringify({
+		"questionsVersionNum": "1.5.0",
+		"questionsList": _OKCP.filteredQuestionsList
+	});
+});
