343e683d35d4d2fad12f5424d2213b086ec1cb69
Ben Jaffe <jaffe.ben@gmail.com>
2014-07-22 19:15:31 -0700

    added sex_worker_positive category

--- plugin/questions/sex.js
+++ plugin/questions/sex.js
@@ -514,6 +514,52 @@ _OKCP.fileQuestions.sex =
 			}
 		],
 
+		"sex_worker_positive": [
+			{
+				"qid": "38508",
+				"text": "Imagine that after becoming romantically involved with someone, they reveal that they formerly earned a living as a prostitute. How would you react to this new information?",
+				"answerText": ["Positively.", "Negatively.", "Indifferently."],
+				"score": [1, -1, 1],
+				"weight": [1, 1, 1]
+			}, {
+				"qid": "18048",
+				"text": "Could you date someone who is/was any sort of sex worker?",
+				"answerText": ["Sure. At least they know what they're doing.","No way!","Depends... what kind of sex worker?","The very notion of this question offends me."],
+				"score": [1, -1, -0.5, 1],
+				"weight": [1, 1, 1, 0.5]
+			}, {
+				"qid": "218",
+				"text": "Should prostitution be legal?",
+				"answerText": ["Yes, absolutely","Yes, only if it were regulated","I don't think so","ABSOLUTELY NOT" ],
+				"score": [1, 0.2, -1, -1],
+				"weight": [1, 0.8, 1, 1]
+			}, {
+				"qid": "333",
+				"text": "Would you ever sleep with a porn star?",
+				"answerText": ["Yes","No"],
+				"score": [1, -1],
+				"weight": [1, 1]
+			}, {
+				"qid": "13",
+				"text": "Is a girl who's slept with 100 guys a bad person?",
+				"answerText": ["Yes","No"],
+				"score": [1, -1],
+				"weight": [1, 1]
+			}, {
+				"qid": "48659",
+				"text": "Would you consider having a serious relationship with an active porn star?",
+				"answerText": ["Yes.","No."],
+				"score": [1, -1],
+				"weight": [1, 1]
+			}, {
+				"qid": "61465",
+				"text": "If you had an eighteen year old son or daughter who wanted to become an adult film star, how would you react?",
+				"answerText": ["I would encourage him or her.","I would discourage him or her.","I would encourage my son, but not my daughter.","I would encourage my daughter, but not my son."],
+				"score": [1, 0, -1, -1],
+				"weight": [1, 0.5, 1, 1]
+			}
+		],
+
 
 		"BDSM": [
 			{
